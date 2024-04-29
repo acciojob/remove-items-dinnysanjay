e330ea599593b57c3244e38ec1326d3ca3a16160
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-04-29 20:46:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let button = document.querySelector('input[type="button"]');
+let dropdown = document.getElementById('colorSelect');
+
+button.addEventListener('click', function() {
+  let selectedOptionIndex = dropdown.selectedIndex;
+  dropdown.remove(selectedOptionIndex);
+});
